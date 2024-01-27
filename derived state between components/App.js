b255350445ac80bf66f38b6e46d4da4597efec86
garo23 <107251338+garo23@users.noreply.github.com>
2024-01-27 18:10:33 +0200

    Create App.js

---
+++ derived state between components/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import ParentComponent from "./Parent.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ParentComponent />
+    </div>
+  );
+}
