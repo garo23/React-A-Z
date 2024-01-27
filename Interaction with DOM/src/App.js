28c709e8bce4290f4bdcaab40b8a92dfe67afdaa
garo23 <107251338+garo23@users.noreply.github.com>
2024-01-27 17:34:44 +0200

    Create App.js

---
+++ Interaction with DOM/src/App.js
@@ -0,0 +1,10 @@
+import DOMInteractionFunctional from "./DOMInteractionFunctional";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <DOMInteractionFunctional />
+    </div>
+  );
+}
