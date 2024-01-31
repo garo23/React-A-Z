2433f5a300b175164965c4adb419b525317aae88
garo23 <107251338+garo23@users.noreply.github.com>
2024-01-31 22:34:19 +0200

    Create App.js

---
+++ container-component/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { books } from "./bookshelf.js";
+import Books from "./Books";
+import Search from "./Search";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleInputChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+  // performs a filtering on the questions
+  // based on the user's input and returns a new array
+  // which gets rendered to the DOM.
+
+  const filteredBooks = books.filter((book) => {
+    return book.name.includes(searchTerm);
+  });
+  return (
+    <section>
+      <Search name="search" onSearch={handleInputChange} val={searchTerm} />
+      <Books books={filteredBooks} />
+    </section>
+  );
+};
+export default App;
