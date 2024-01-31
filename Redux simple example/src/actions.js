f0fc46df045cef7d162ee75d75082f9a8863ca40
garo23 <107251338+garo23@users.noreply.github.com>
2024-01-31 22:24:08 +0200

    Create actions.js

---
+++ Redux simple example/src/actions.js
@@ -0,0 +1,26 @@
+// Action Types
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+// Action Creators
+export const increment = () => ({
+  type: INCREMENT
+});
+
+export const decrement = () => ({
+  type: DECREMENT
+});
+
+// Reducer
+const initialState = { count: 0 };
+
+export const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 };
+    case DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
