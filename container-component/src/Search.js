6c3ca881fcf52dd197b1a30b7f782d6240a4f9ae
garo23 <107251338+garo23@users.noreply.github.com>
2024-01-31 22:35:11 +0200

    Create Search.js

---
+++ container-component/src/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Search = ({ val, onSearch, ...props }) => {
+  return (
+    <input
+      value={val}
+      onChange={onSearch}
+      placeholder="Search for any book"
+      {...props}
+    />
+  );
+};
+
+export default Search;
